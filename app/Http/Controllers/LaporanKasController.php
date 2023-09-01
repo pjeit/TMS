7253dad7392006c939b019a9695b92e973750708
Edwin Aritama <aritamaedwin@gmail.com>
2023-09-01 16:53:08 +0700

    kas done

--- app/Http/Controllers/LaporanKasController.php
+++ app/Http/Controllers/LaporanKasController.php
@@ -15,38 +15,95 @@ class LaporanKasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-        $data = DB::table('kas_bank_transaction')
-            ->where('is_aktif', '=', 'Y') 
-            ->where('id_kas_bank', '3') 
-            ->whereBetween('tanggal', ['2023-07-01', '2023-07-31'])
-            ->orderBy('tanggal', 'ASC')
-            ->get();
+        $tanggal_awal   = $request->input('tanggal_awal');
+        $tanggal_akhir  = $request->input('tanggal_akhir');
 
-        $kas = DB::table('kas_bank')
-            ->where('id', '3') 
-            ->first();
+        if(strlen($tanggal_awal) && strlen($tanggal_akhir)){
+            $tgl_default = '2000-01-01';
+            $tgl_awal = date('Y-m-d', strtotime($tanggal_awal)); // date_create_from_format('d-M-Y', $tanggal_awal);
+            $tgl_akhir = date('Y-m-d', strtotime($tanggal_akhir)); // date_create_from_format('d-M-Y', $tanggal_awal);
+    
+            $data = DB::table('kas_bank_transaction')
+                ->where('is_aktif', '=', 'Y') 
+                ->where('id_kas_bank', '3') 
+                ->whereBetween('tanggal', [$tgl_awal, $tgl_akhir])
+                ->orderBy('tanggal', 'ASC')
+                ->get();
+    
+            // buat ngitung total, biar ngitungnya ga di frontend
+                DB::statement('set @kas_bank_id = 0');
+                DB::statement('set @subtotal = 0');
+            //
+            $data = DB::select("SELECT d.tanggal, d.jenis, d.keterangan_transaksi, d.kode_coa, d.debit, d.kredit, d.id
+                ,if(@kas_bank_id <> d.id_kas_bank, 
+                    @subtotal := ifnull(d.debit,0) - ifnull(d.kredit,0), 
+                    @subtotal := ifnull(@subtotal,0) + ifnull(d.debit,0) - ifnull(d.kredit,0)
+                ) as total,
+                if(@subtotal >= 0, abs(@subtotal), 0) as subtotal_debit,
+                if(@subtotal >= 0, 0, abs(@subtotal)) as subtotal_kredit,
+                @kas_bank_id := d.id_kas_bank as xx, d.id_kas_bank as idx
+                FROM (
+                    SELECT 
+                    id, id_kas_bank, CAST('$tgl_awal' AS DATE) AS tanggal, NULL AS jenis, 
+                    'Saldo Awal' AS keterangan_transaksi, NULL AS kode_coa, 
+                    IF(SUM(debit) - SUM(kredit) >= 0, ABS(SUM(debit) - SUM(kredit)), 0) AS debit,
+                    IF(SUM(debit) - SUM(kredit) >= 0, 0, ABS(SUM(debit) - SUM(kredit))) AS kredit
+                    FROM kas_bank_transaction 
+                    WHERE id_kas_bank = '3'
+                    AND CAST(tanggal AS DATE) BETWEEN '$tgl_default' AND '$tgl_awal'
+                    AND is_aktif = 'Y'
+                    group by id, id_kas_bank, tanggal, jenis, keterangan_transaksi, kode_coa, debit, kredit
+                    UNION ALL
+                    SELECT 
+                        id, id_kas_bank, tanggal, jenis, keterangan_transaksi, kode_coa, debit, kredit
+                    FROM kas_bank_transaction 
+                    WHERE id_kas_bank = '3'
+                    AND CAST(tanggal AS DATE) BETWEEN '$tgl_awal' AND '$tgl_akhir'
+                    AND is_aktif = 'Y'
+                    group by id, id_kas_bank, tanggal, jenis, keterangan_transaksi, kode_coa, debit, kredit
+                ) AS d 
+                ORDER BY CAST(d.tanggal AS DATE)
+            ");
 
-        $sumKredit = DB::table('kas_bank_transaction')
-            ->where('is_aktif', '=', 'Y') 
-            ->where('id_kas_bank', '3') 
-            ->whereBetween('tanggal', ['2023-07-01', '2023-07-31'])
-            ->sum('kredit');
-        $sumDebit = DB::table('kas_bank_transaction')
-            ->where('is_aktif', '=', 'Y') 
-            ->where('id_kas_bank', '3') 
-            ->whereBetween('tanggal', ['2023-07-01', '2023-07-31'])
-            ->sum('debit');
-
-        return view('pages.laporan.Kas.index',[
-            'judul' => "LAPORAN KAS",
-            'data' => $data,
-            'kas' => $kas,
-            'sumKredit' => $sumKredit,
-            'sumDebit' => $sumDebit,
-        ]);
+            $kas = DB::table('kas_bank')->where('id', '3') ->first();
+    
+            $sumKredit = DB::table('kas_bank_transaction')
+                ->where('is_aktif', '=', 'Y') 
+                ->where('id_kas_bank', '3') 
+                // ->where('tanggal', '<=', '2023-06-01')
+                ->whereBetween('tanggal', [$tgl_awal, $tgl_akhir])
+                ->sum('kredit');
+    
+            $sumDebit = DB::table('kas_bank_transaction')
+                ->where('is_aktif', '=', 'Y') 
+                ->where('id_kas_bank', '3') 
+                // ->where('tanggal', '<=', '2023-06-01')
+                ->whereBetween('tanggal', [$tgl_awal, $tgl_akhir])
+                ->sum('debit');
+    
+            return view('pages.laporan.Kas.index',[
+                'judul' => "LAPORAN KAS",
+                'data' => $data,
+                'kas' => $kas,
+                'sumKredit' => $sumKredit,
+                'sumDebit' => $sumDebit,
+            ]);
+        }else{
+            $data = NULL;
+            $kas = NULL;
+            $sumKredit = NULL;
+            $sumDebit = NULL;
+            return view('pages.laporan.Kas.index',[
+                'judul' => "LAPORAN KAS",
+                'data' => $data,
+                'kas' => $kas,
+                'sumKredit' => $sumKredit,
+                'sumDebit' => $sumDebit,
+            ]);
+        }
+       
     }
 
     /**
--- resources/views/pages/laporan/Kas/index.blade.php
+++ resources/views/pages/laporan/Kas/index.blade.php
@@ -17,67 +17,102 @@
    
 </style>
 <div class="container-fluid">
-    <div class="row">
-        <section class="col-lg-12">
-            <!-- Custom tabs (Charts with tabs)-->
-            <div class="card radiusSendiri" style="position: relative; left: 0px; top: 0px; background-color:#edf4fc;">
-            <div class="card-header ui-sortable-handle">
-                <input type="hidden" value="https://testapps.pjexpress.co.id/index.php/C_lap_kas/export_to_excel" id="url_download">
-                <form id="form_report" action="https://testapps.pjexpress.co.id/index.php/c_lap_kas/get_laporan">
-                    <div class="form-group">
-                        <label for="periode">Periode</label>
-                        <div class="col-lg-12">
-                            <div class="row">
-                                <input type="text" name="tanggal_awal" autocomplete="off" class="date form-control" id="tanggal_awal" placeholder="dd-M-yyyy" style="width:120px;text-align:center">
-                                <label for="periode">&nbsp; s/d &nbsp;</label>  
-                                <input type="text" name="tanggal_akhir" autocomplete="off" class="date form-control" id="tanggal_akhir" placeholder="dd-M-yyyy" style="width:120px;text-align:center">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <button type="button" class="btn btn-sm btn-primary" onclick="show_report()"><i class="fas fa-search"></i> Tampilkan Data</button>
-                        <button type="button" class="btn btn-sm btn-success" onclick="download_report()"><i class="fas fa-file-excel"></i> Export to Excel</button>
-                    </div>
-                </form>
-            </div><!-- /.card-header -->
-            </div>
-            <!-- /.card -->
-        </section>
+    <div class="card">
+        {{-- <div class="row"> --}}
+            <div class="card-header ">
+                {{-- <div class="" style="position: relative; left: 0px; top: 0px; background-color:#edf4fc;"> --}}
+                    <div class="card-header">
+                            <form id="form_report" action="{{ route('laporan_kas.index') }}" method="GET">
+                                <div class="form-group">
+                                    <label for="periode">Periode</label>
+                                    <div class="col-lg-12">
+                                        <div class="row">
+                                            <div class="form-group">
+                                                <div class="input-group mb-0">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                                                    </div>
+                                                    <input type="text" name="tanggal_awal" autocomplete="off" class="date  form-control" id="tanggal_awal" placeholder="dd-M-yyyy" value="{{ $request['tanggal_awal'] ?? '' }}">     
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="periode" class="mt-2 ml-2 mr-2">s/d</label>  
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group mb-0">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+                                                    </div>
+                                                    <input type="text" name="tanggal_akhir" autocomplete="off" class="date  form-control" id="tanggal_akhir" placeholder="dd-M-yyyy" value="{{ $request['tanggal_akhir'] ?? '' }}">     
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-sm btn-primary ml-3 mt-1 radiusSendiri" onclick="show_report()"><i class="fas fa-search"></i> <b>Tampilkan Data</b></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
 
-        <section class="col-lg-12" id="show_report">
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th style="width:1px; white-space: nowrap;">Tgl. Transaksi</th>
-                        <th>Jenis</th>
-                        <th>Deskripsi</th>
-                        <th style="width:1px; white-space: nowrap; text-align:right;">Debit</th>
-                        <th style="width:1px; white-space: nowrap; text-align:right;">Kredit</th>
-                        <th style="width:1px; white-space: nowrap; text-align:right;">Saldo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        @if (isset($data))
-                            <tr>
-                                <td colspan="7">KAS KECIL {{$kas->saldo_sekarang}} {{$sumDebit}} {{$sumKredit}} ({{number_format($kas->saldo_sekarang + $sumDebit - $sumKredit)}})</td>
-                            </tr>
-                            @foreach ($data as $item)
-                            <tr>
-                                <td></td>
-                                <td>{{$item->tanggal}}</td>
-                                <td>{{$item->kode_coa}}</td>
-                                <td>{{$item->keterangan_transaksi}}</td>
-                                <td>{{number_format($item->debit)}}</td>
-                                <td>{{number_format($item->kredit)}}</td>
-                                <td></td>
-                            </tr>
-                            @endforeach
-                        @endif
-                </tbody>
-            </table>
-        </section>
+                            <div class="form-group">
+                                {{-- <button type="button" class="btn btn-sm btn-success" onclick="download_report()"><i class="fas fa-file-excel"></i> Export to Excel</button> --}}
+                            </div>
+                    </div><!-- /.card-header -->
+                {{-- </div> --}}
+            </div>
+            
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th style="width:1px; white-space: nowrap;">Tgl. Transaksi</th>
+                            <th>Jenis</th>
+                            <th>Keterangan</th>
+                            <th style="width:1px; white-space: nowrap; text-align:right;">Debit</th>
+                            <th style="width:1px; white-space: nowrap; text-align:right;">Kredit</th>
+                            <th style="width:1px; white-space: nowrap; text-align:right;">Saldo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            @if (isset($data))
+                                <tr>
+                                    {{-- <td colspan="7">KAS KECIL {{number_format($kas->saldo_sekarang)}} | DEBIT: {{number_format($sumDebit)}} | KREDIT: {{number_format($sumKredit)}} | TOT SKRG: ({{number_format($kas->saldo_sekarang + $sumDebit - $sumKredit)}})</td> --}}
+                                    <td colspan="7">KAS KECIL (Saldo: {{number_format($kas->saldo_sekarang + $sumDebit - $sumKredit)}})</td>
+                                </tr>
+                                @foreach ($data as $item)
+                                <tr>
+                                    <td></td>
+                                    <td>{{date('d-M-Y', strtotime($item->tanggal)) }}</td>
+                                    <td>{{$item->jenis}}</td>
+                                    <td>{{$item->keterangan_transaksi}}</td>
+                                    <td>{{number_format($item->debit)}}</td>
+                                    <td>{{number_format($item->kredit)}}</td>
+                                    <td>{{number_format($item->total)}}</td>
+                                </tr>
+                                @endforeach
+                            @endif
+                    </tbody>
+                </table>
+            </div>
+        {{-- </div> --}}
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#tanggal_awal').datepicker({
+            autoclose: true,
+            format: "dd-M-yyyy",
+            todayHighlight: true,
+            language:'en',
+        });
+        $('#tanggal_akhir').datepicker({
+            autoclose: true,
+            format: "dd-M-yyyy",
+            todayHighlight: true,
+            language:'en',
+        });
+    });
 
+</script>
 @endsection
