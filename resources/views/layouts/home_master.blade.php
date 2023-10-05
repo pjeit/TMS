1524f5f19901af15669ed225f9efb1017d66e28a
Edwin Aritama <aritamaedwin@gmail.com>
2023-10-05 20:47:28 +0700

    .

--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -186,16 +186,78 @@
           @if(session("status"))
             {{-- sweetalert --}}
             @include('sweetalert::alert')
-            {{-- <div class="alert alert-success alert-dismissible fade show" role="alert">
-              {{session('status')}}
-  
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
+            <div class="container-fluid">
+              {{-- <div aria-labelledby="swal2-title" aria-describedby="swal2-html-container" class="radiusSendiri mt-0 mb-2 timerProgressBar swal2-popup swal2-toast swal2-icon-success swal2-show" tabindex="-1" role="alert" aria-live="polite" style="width: 100%; display: grid;">
+                  <button type="button" class="swal2-close" aria-label="Close this dialog" style="display: block;">×</button>
+                  <ul class="swal2-progress-steps" style="display: block;"></ul>
+                  <div class="swal2-loader"></div>
+                  <div class="swal2-icon swal2-success swal2-icon-show" style="display: flex;">
+                      <div class="swal2-success-circular-line-left" style="background-color: rgb(255, 255, 255);"></div>
+                      <span class="swal2-success-line-tip"></span>
+                      <span class="swal2-success-line-long"></span>
+                      <div class="swal2-success-ring"></div>
+                      <div class="swal2-success-fix" style="background-color: rgb(255, 255, 255);"></div>
+                      <div class="swal2-success-circular-line-right" style="background-color: rgb(255, 255, 255);"></div>
+                  </div>
+                  <img class="swal2-image" style="display: none;">
+                  <h2 class="swal2-title" id="swal2-title" style="display: block;">
+                    {{session('status')}}
+                  </h2>
+                  <div class="swal2-html-container" id="swal2-html-container" style="display: none;"></div>
+                  <input id="swal2-input" class="swal2-input" style="display: none;">
+                  <input type="file" class="swal2-file" style="display: none;">
+                  <div class="swal2-range" style="display: none;"><input type="range"><output></output></div>
+                  <select id="swal2-select" class="swal2-select" style="display: none;"></select>
+                  <div class="swal2-radio" style="display: none;"></div>
+                  <label class="swal2-checkbox" style="display: none;">
+                      <input type="checkbox" id="swal2-checkbox">
+                      <span class="swal2-label"></span>
+                  </label>
+                  <textarea id="swal2-textarea" class="swal2-textarea" style="display: none;"></textarea>
+                  <div class="swal2-validation-message" id="swal2-validation-message" style="display: none;"></div>
+                  <div class="swal2-actions" style="display: none;">
+                      <button type="button" class="swal2-confirm swal2-styled" aria-label="" style="display: none;">OK</button>
+                      <button type="button" class="swal2-deny swal2-styled" aria-label="" style="display: none;">No</button>
+                      <button type="button" class="swal2-cancel swal2-styled" aria-label="" style="display: none;">Cancel</button>
+                  </div>
+                  <div class="swal2-footer" style="display: block;"></div>
+                  <div class="swal2-timer-progress-bar-container">
+                      <div class="swal2-timer-progress-bar" style="display: flex; width: 12.0588%;">
+                  </div>
+                </div>
+              </div> --}}
+              {{-- <template id="my-template">
+                <swal-title>
+                  Save changes to "Untitled 1" before closing?
+                </swal-title>
+                <swal-icon type="warning" color="red"></swal-icon>
+                <swal-button type="confirm">
+                  Save As
+                </swal-button>
+                <swal-button type="cancel">
+                  Cancel
+                </swal-button>
+                <swal-button type="deny">
+                  Close without Saving
+                </swal-button>
+                <swal-param name="allowEscapeKey" value="false" />
+                <swal-param
+                  name="customClass"
+                  value='{ "popup": "my-popup" }' />
+                <swal-function-param
+                  name="didOpen"
+                  value="popup => console.log(popup)" />
+              </template>
             </div> --}}
-            <div class="" id="popup-alert">
 
-            </div>
+              <div id="toast-container" class="container">
+                Toast Should be here?
+              </div>  
+
+
+            {{-- <div class="swal2-show swal2-backdrop-show swal2-icon-show" style="background-color: #fff;" role="alert">
+              {{session('status')}}
+            </div> --}}
 
           @endif
 
@@ -229,10 +291,15 @@
         if (sessionMessage != '') {
             const Toast = Swal.mixin({
                 toast: true,
-                position: 'center',
+                position: 'top',
+                // width: '1400px',
                 timer: 2500,
                 showConfirmButton: false,
                 timerProgressBar: true,
+                target: document.querySelector('#toast-container'),
+                onOpen: function () {
+                  alert('The ID of my parent is: ' + swal.getContainer().parentElement.id)
+                },
                 didOpen: (toast) => {
                     toast.addEventListener('mouseenter', Swal.stopTimer)
                     toast.addEventListener('mouseleave', Swal.resumeTimer)
@@ -249,9 +316,17 @@
                 var iconData = 'success';
                 var titleData = '';
             }
+            // Toast.fire({
+            //     icon: iconData,
+            //     title: titleData
+            // })
+            // Swal.fire({
+            //   template: '#my-template'
+            // })
+
             Toast.fire({
-                icon: iconData,
-                title: titleData
+              title: 'Toast in a custom container',
+              toast: true,
             })
         }
     });
