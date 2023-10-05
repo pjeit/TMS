9fa9c131b00ba1530911e405fcd848726d74ef8f
Edwin Aritama <aritamaedwin@gmail.com>
2023-10-05 15:44:12 +0700

    .

--- resources/views/layouts/home_master.blade.php
+++ resources/views/layouts/home_master.blade.php
@@ -236,10 +236,19 @@
                 didOpen: (toast) => {
                     toast.addEventListener('mouseenter', Swal.stopTimer)
                     toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
+              }
             })
-            var iconData = sessionMessage == 'Success'? 'success':'danger';
-            var titleData = sessionMessage == 'Success'? 'Data tersimpan!':'Data gagal disimpan!';
+
+            if (/^Success/.test(sessionMessage) || /^Sukses/.test(sessionMessage)) {
+                var iconData = 'success';
+                var titleData = 'Data tersimpan!';
+            } else if (/^Error/.test(sessionMessage)) {
+                var iconData = 'danger';
+                var titleData = 'Data gagal disimpan!';
+              }else{
+                var iconData = 'success';
+                var titleData = '';
+            }
             Toast.fire({
                 icon: iconData,
                 title: titleData
