82d492d113ee709523f8e59c9c61e527a47c9977
Edwin Aritama <aritamaedwin@gmail.com>
2023-10-09 13:48:00 +0700

    .

--- resources/views/pages/finance/add_return_TL/index.blade.php
+++ resources/views/pages/finance/add_return_TL/index.blade.php
@@ -88,79 +88,71 @@
     </div>
 </div>
 <script>
-    // $(document).ready(function() {
-    //     var formElement = document.querySelector("#form_report");
-    //     var formData = new FormData(formElement);
-    //     $("#loading-spinner").show();
-    //     // showTable(formData);
+    $(document).ready(function() {
+        var status = $('#status_tl').val();
+        showTable(status);
 
-    //     $(document).on('click','#btnKu',function(e){
-    //         var formElement = document.querySelector("#form_report");
-    //         var formData = new FormData(formElement);
-    //         $("#loading-spinner").show();
-    //         showTable(formData);
-	// 	});
-        
-    //     function showTable(formData){
-    //         $.ajax({
-    //             method: 'POST',
-    //             url: '{{ route('pembayaran_sdt.load_data') }}',
-    //             data: formData,
-    //             dataType: 'JSON',
-    //             contentType: false,
-    //             cache: false,
-    //             processData:false,
-    //             success: function(response) {
-    //                 $("#loading-spinner").hide();
-    //                 console.log(response);
-    //                 var data = response.data;
-    //                 console.log('data '+data);
-    //                     $("#hasil").html(" ");
+        $(document).on('change', '#status_tl', function(e) {  
+            showTable(this.value)
+		});        
 
-    //                     var nyimpenIdBapakJO = null;
-    //                     for (var i = 0; i < data.length; i++) {
-    //                         if (data[i].id_jo !== nyimpenIdBapakJO) {
-    //                             var row = $("<tr></tr>");
-    //                             row.append(`<td style='background: #efefef'> <a class="btn btn-sm btn-primary radiusSendiri" href="{!! url('/pembayaran_sdt/${data[i].id_jo}/edit') !!}">
-    //                                     <span class="fa fa-share" ></span> 
-    //                                 </a> </td>`)
-    //                             row.append("<td colspan='5' style='background: #efefef'>" + "No. JO : " + data[i].no_jo + "<br> No. BL : " + data[i].no_bl + "</td>");
-    //                             $("#hasil").append(row);
-    //                             nyimpenIdBapakJO = data[i].id_jo;
-    //                         }
+        function showTable(status){
+            $.ajax({
+                method: 'GET',
+                url: `add_return_tl/getData/${status}`,
+                dataType: 'JSON',
+                contentType: false,
+                cache: false,
+                processData:false,
+                success: function(response) {
+                    var table = $('#datatable').DataTable();
+                    table.clear().destroy();
 
-    //                         var row = $("<tr></tr>");
-    //                         // row.append(`
-    //                         //     <td>
-    //                         //         <a class="btn btn-sm btn-primary radiusSendiri" href="{!! url('/pembayaran_sdt/${data[i].id}/edit') !!}">
-    //                         //             <span class="fas fa-edit" ></span> <b></b>
-    //                         //         </a>
-    //                         //     </td>
-    //                         // `); 
-                            
-    //                         row.append("<td> </td>");
-    //                         row.append("<td>" + data[i].no_kontainer + "</td>");
-    //                         row.append("<td>" + data[i].kode + " - " + data[i].nama_cust + "</td>");
-    //                         row.append("<td>" + data[i].nama_supp + "</td>");
-    //                         row.append("<td>" + data[i].statusDetail + "</td>");
-                            
-    //                         $("#hasil").append(row);
-    //                     }
-    //             },error: function (xhr, status, error) {
-    //                     $("#loading-spinner").hide();
-    //                 if ( xhr.responseJSON.result == 'error') {
-    //                     console.log("Error:", xhr.responseJSON.message);
-    //                     console.log("XHR status:", status);
-    //                     console.log("Error:", error);
-    //                     console.log("Response:", xhr.responseJSON);
-    //                 } else {
-    //                     toastr.error("Terjadi kesalahan saat menerima data. " + error);
-    //                 }
-    //             }
-    //         });
-    //     }
-       
-    // });
+                    $("#hasil").append(row);
+                    $("#loading-spinner").hide();
+                    var data = response;
+                    console.log('response', data);
+                    // for (var i = 0; i < data.length; i++) {
+                    //     var row = $("<tr></tr>");
+                    //     row.append(`<td>${data[i].nama_customer}</td>`);
+                    //     row.append(`<td>${data[i].no_polisi}</td>`);
+                    //     row.append(`<td>${data[i].no_sewa}</td>`);
+                    //     row.append(`<td>${data[i].tanggal_berangkat}</td>`);
+                    //     row.append(`<td>${data[i].alamat_tujuan}</td>`);
+                    //     row.append(`<td>${data[i].nama_driver}</td>`);
+                    //     row.append(`<td>${data[i].status}</td>`);
+                    //     row.append(`<td class='text-center'> 
+                    //                     <div class="btn-group dropleft">
+                    //                         <button type="button" class="btn btn-rounded btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    //                             <i class="fa fa-list"></i>
+                    //                         </button>
+                    //                         <div class="dropdown-menu">
+                    //                             <a href="truck_order/${data[i].id_sewa}/edit" class="dropdown-item">
+                    //                                 <span class="fas fa-edit mr-3"></span> Edit
+                    //                             </a>
+                    //                             <a href="truck_order/destroy/${data[i].id_sewa}" class="dropdown-item" data-confirm-delete="true">
+                    //                                 <span class="fas fa-trash mr-3"></span> Delete
+                    //                             </a>
+                    //                         </div>
+                    //                     </div>
+                    //                 </td>`);
+                    //     $("#hasil").append(row);
+                    //     $("#datatable").dataTable();
+                    // }
 
+                },error: function (xhr, status, error) {
+                    $("#loading-spinner").hide();
+                    if ( xhr.responseJSON.result == 'error') {
+                        console.log("Error:", xhr.responseJSON.message);
+                        console.log("XHR status:", status);
+                        console.log("Error:", error);
+                        console.log("Response:", xhr.responseJSON);
+                    } else {
+                        toastr.error("Terjadi kesalahan saat menerima data. " + error);
+                    }
+                }
+            });
+        };
+    });
 </script>
 @endsection
