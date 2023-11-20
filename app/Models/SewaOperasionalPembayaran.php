e45b178ef2c049b3a302916113b9643c29aa4245
Edwin Aritama <aritamaedwin@gmail.com>
2023-11-20 10:22:46 +0700

    revisi pembayaran biaya operasional

---
+++ app/Models/SewaOperasionalPembayaran.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SewaOperasionalPembayaran extends Model
+{
+    use HasFactory;
+    protected $table = 'sewa_operasional_pembayaran';
+    protected $primaryKey ='id';
+
+    public function getOperasional()
+    {
+        return $this->hasMany(SewaOperasional::class, 'id_pembayaran', 'id')->where('is_aktif', 'Y');
+    }
+}
