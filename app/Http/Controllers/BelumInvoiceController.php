5d56a5179707364cb8782fd8c3c143069bac82c7
Motzz <s160419007@student.ubaya.ac.id>
2023-12-07 13:27:41 +0700

    .

--- app/Http/Controllers/BelumInvoiceController.php
+++ app/Http/Controllers/BelumInvoiceController.php
@@ -76,7 +76,7 @@ class BelumInvoiceController extends Controller
             $sewa = session()->get('sewa'); //buat ambil session
             $cust = session()->get('cust'); //buat ambil session
             $grup = session()->get('grup'); //buat ambil session
-            Session::forget(['sewa', 'cust', 'grup']);
+            // Session::forget(['sewa', 'cust', 'grup']);
 
             $data= $request->collect();
             session()->put('sewa', $data['idSewa']);
