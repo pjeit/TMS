238ab0d03f313b010fc4c230f96cbfbc33c0e698
Motzz <s160419007@student.ubaya.ac.id>
2024-02-12 14:28:13 +0700

    dasboard 12 feb

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -120,69 +120,69 @@
     <!-- Left col -->
     <section class="col-lg-12">
         <!-- Custom tabs (Charts with tabs)-->
-        <div class="card" style="position: relative; left: 0px; top: 0px;" id='table_jadwal'>
-        <div class="card-header">
-            <h3 class="card-title" style='font-size:12pt;'>
-            <i class="fas fa-calendar mr-1"></i>Periode: {{$week_start}} s/d {{$week_end}}
-            </h3>
-            <div class=sr-only>
-                <input type='hidden' id="prev_week" value="{{ $weeknumber-1}}">
-                <input type='hidden' id="next_week" value="{{ $weeknumber+1}}">
-            </div>
-            <div class="card-tools">
-            <ul class="nav nav-pills ml-auto">
-                <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)" onclick="get_jadwal('prev')" style='border:1px'><i class="fas fa-caret-left"></i></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)" onclick="get_jadwal('next')" style='border:1px'><i class="fas fa-caret-right"></i></a>
-                </li>
-            </ul>
-            </div>
-        </div><!-- /.card-header -->
-        <div class="card-body">
-            <span style='font-size:11pt;' class="badge bg-primary">Menunggu Persetujuan</span> <span style='font-size:11pt;' class="badge bg-warning">Menunggu Uang Jalan</span>  <span style='font-size:11pt;' class="badge bg-success">Dalam Perjalanan</span>  <span style='font-size:11pt;' class="badge bg-info">Selesai</span>  <span style='font-size:11pt;' class="badge bg-danger">Maintenance</span> <span style='font-size:11pt;' class="badge bg-dark">Batal Muat</span>    
-            <table class="default_table table table-bordered table-striped">
-                <thead>
-                <tr>
-                <th style="width:12%">Nopol</th>
-                <th style="width:11%">Driver</th>
-                <th style="width:11%">Senin</th>
-                <th style="width:11%">Selasa</th>
-                <th style="width:11%">Rabu</th>
-                <th style="width:11%">Kamis</th>
-                <th style="width:11%">Jumat</th>
-                <th style="width:11%">Sabtu</th>
-                <th style="width:11%">Minggu</th>
-                </tr>
-                </thead>
-                <!--<tbody>
-                    <?php if(!empty($data)){
-                    foreach($data as $kendaraan=>$jadwal){$info=explode('_',$kendaraan);?>
-                        <tr>
-                            <td><?= $info[0];?></td>
-                            <td><?= $info[1];?></td>
-                            <?php foreach($jadwal as $hari=>$value){ ?>
-                            <td>
-                            <?php foreach($value as $objek){
-                                $color='';
-                                if(strtolower($objek->status)=='open'){$color='bg-info';}
-                        elseif(strtolower($objek->status)=='approved' && $objek->total_uang_jalan > 0){$color='bg-warning';}
-                        elseif(strtolower($objek->status)=='released' || (strtolower($objek->status)=='approved' && $objek->total_uang_jalan == 0)){$color='bg-success';}
-                                elseif(strtolower($objek->status)=='finished'){$color='bg-info';}
-                                elseif(strtolower($objek->status)=='maintenance'){$color='bg-danger';}
-                                                            elseif(strtolower($objek->status)=='canceled'){$color='bg-dark';}
-                            ?>
-                                <span class="badge <?= $color; ?>">
-                                    <?php if($objek->nama_tujuan!='' && !empty($objek->nama_tujuan)){echo $objek->nama_tujuan;}?></span>
-                            <?php }?>
-                            </td>
-                            <?php }?>
-                        </tr>
-                    <?php }}?>
-                </tbody>-->
-            </table>
-        </div><!-- /.card-body -->
+        <div class="card radiusSendiri" style="position: relative; left: 0px; top: 0px;" id='table_jadwal'>
+            <div class="card-header">
+                <h3 class="card-title" style='font-size:12pt;'>
+                <i class="fas fa-calendar mr-1"></i>Periode: {{$week_start}} s/d {{$week_end}}
+                </h3>
+                <div class=sr-only>
+                    <input type='hidden' id="prev_week" value="{{ $weeknumber-1}}">
+                    <input type='hidden' id="next_week" value="{{ $weeknumber+1}}">
+                </div>
+                <div class="card-tools">
+                <ul class="nav nav-pills ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)" onclick="get_jadwal('prev')" style='border:1px'><i class="fas fa-caret-left"></i></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0)" onclick="get_jadwal('next')" style='border:1px'><i class="fas fa-caret-right"></i></a>
+                    </li>
+                </ul>
+                </div>
+            </div><!-- /.card-header -->
+            <div class="card-body">
+                <span style='font-size:11pt;' class="badge bg-primary">Menunggu Persetujuan</span> <span style='font-size:11pt;' class="badge bg-warning">Menunggu Uang Jalan</span>  <span style='font-size:11pt;' class="badge bg-success">Dalam Perjalanan</span>  <span style='font-size:11pt;' class="badge bg-info">Selesai</span>  <span style='font-size:11pt;' class="badge bg-danger">Maintenance</span> <span style='font-size:11pt;' class="badge bg-dark">Batal Muat</span>    
+                <table class="default_table table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                    <th style="width:12%">Nopol</th>
+                    <th style="width:11%">Driver</th>
+                    <th style="width:11%">Senin</th>
+                    <th style="width:11%">Selasa</th>
+                    <th style="width:11%">Rabu</th>
+                    <th style="width:11%">Kamis</th>
+                    <th style="width:11%">Jumat</th>
+                    <th style="width:11%">Sabtu</th>
+                    <th style="width:11%">Minggu</th>
+                    </tr>
+                    </thead>
+                    <!--<tbody>
+                        <?php if(!empty($data)){
+                        foreach($data as $kendaraan=>$jadwal){$info=explode('_',$kendaraan);?>
+                            <tr>
+                                <td><?= $info[0];?></td>
+                                <td><?= $info[1];?></td>
+                                <?php foreach($jadwal as $hari=>$value){ ?>
+                                <td>
+                                <?php foreach($value as $objek){
+                                    $color='';
+                                    if(strtolower($objek->status)=='open'){$color='bg-info';}
+                            elseif(strtolower($objek->status)=='approved' && $objek->total_uang_jalan > 0){$color='bg-warning';}
+                            elseif(strtolower($objek->status)=='released' || (strtolower($objek->status)=='approved' && $objek->total_uang_jalan == 0)){$color='bg-success';}
+                                    elseif(strtolower($objek->status)=='finished'){$color='bg-info';}
+                                    elseif(strtolower($objek->status)=='maintenance'){$color='bg-danger';}
+                                                                elseif(strtolower($objek->status)=='canceled'){$color='bg-dark';}
+                                ?>
+                                    <span class="badge <?= $color; ?>">
+                                        <?php if($objek->nama_tujuan!='' && !empty($objek->nama_tujuan)){echo $objek->nama_tujuan;}?></span>
+                                <?php }?>
+                                </td>
+                                <?php }?>
+                            </tr>
+                        <?php }}?>
+                    </tbody>-->
+                </table>
+            </div><!-- /.card-body -->
         </div>
         <!-- /.card -->
     </section>
